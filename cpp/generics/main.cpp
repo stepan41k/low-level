56695d64fb234bb3db1cd8a2acf465fbe7c5e44a
stepna41k <russia.zont@gmail.com>
2026-06-11 23:58:16 +0300

    feat: add generics example

---
+++ cpp/generics/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+T myMax(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    std::cout << myMax(3, 7) << std::endl;        // int
+    std::cout << myMax(3.1, 7.8) << std::endl;    // double
+    std::cout << myMax(std::string("hello"), std::string("world")) << std::endl; // string
+    return 0;
+}
